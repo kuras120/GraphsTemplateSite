da35febfb2f225aa87da75aca3d3907039c91496
Wojciech Kur <wojciech.kur96@gmail.com>
2020-09-26 18:03:04 +0200

    gunicorn instead of embedded server

--- run.sh
+++ run.sh
@@ -10,4 +10,4 @@ cd server || exit
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-python manage.py runserver localhost:8000
+gunicorn --bind 0.0.0.0:8000 entrypoint.wsgi:application
