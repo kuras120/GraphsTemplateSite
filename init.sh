79b764d97169edb151836f086232b6c39e19e4db
Wojciech <wojciech.kur96@gmail.com>
2023-09-09 19:21:10 +0200

    legit compose plugin

--- init.sh
+++ init.sh
@@ -5,11 +5,11 @@ if [ $# -eq 0 ]
     read -n 1 -s -r -p "Press any key to continue"$'\n'
     exit
 fi
-docker-compose -f compose-"$1".yml --env-file .env down -v
-docker-compose -f compose-"$1".yml --env-file .env up -d --build
-docker-compose -f compose-"$1".yml exec web python /app/server/manage.py makemigrations
-docker-compose -f compose-"$1".yml exec web python /app/server/manage.py migrate # if error, try 'docker-compose down -v' and rebuild
-docker-compose -f compose-"$1".yml exec web python /app/server/manage.py collectstatic --noinput
-docker-compose -f compose-"$1".yml exec web python /app/server/manage.py createsuperuser --noinput
-docker-compose -f compose-"$1".yml exec db psql -f /app/data.sql
+docker compose -f compose-"$1".yml --env-file .env down -v
+docker compose -f compose-"$1".yml --env-file .env up -d --build
+docker compose -f compose-"$1".yml exec web python /app/server/manage.py makemigrations
+docker compose -f compose-"$1".yml exec web python /app/server/manage.py migrate # if error, try 'docker-compose down -v' and rebuild
+docker compose -f compose-"$1".yml exec web python /app/server/manage.py collectstatic --noinput
+docker compose -f compose-"$1".yml exec web python /app/server/manage.py createsuperuser --noinput
+docker compose -f compose-"$1".yml exec db psql -f /app/data.sql
 read -n 1 -s -r -p "Press any key to continue"$'\n'
